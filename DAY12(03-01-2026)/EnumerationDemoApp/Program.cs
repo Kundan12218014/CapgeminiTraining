d5aab920b8c42803935639d4cbe3ef71db0637d8
Kundan Kumar <kundankumar08950@gmail.com>
2026-01-03 15:28:10 +0530

    Enumeration 03-01-2026

---
+++ DAY12(03-01-2026)/EnumerationDemoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+public class MyCollection: IEnumerable
+{
+  public int []data={1,2,3};
+  public IEnumerator GetEnumerator()
+  {
+    return data.GetEnumerator();
+  }
+}
+public class Program
+{
+  public static void Main()
+  {
+    MyCollection obj=new MyCollection();
+    foreach(int i in obj)
+    {
+      Console.Write(i+" ");
+    }
+  }
+}
