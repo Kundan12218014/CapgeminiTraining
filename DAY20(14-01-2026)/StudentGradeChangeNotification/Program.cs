040aa62daeaa18721f489353fbc82372f07ec2de
Kundan Kumar <kundankumar08950@gmail.com>
2026-01-15 15:04:11 +0530

    Events calling

---
+++ DAY20(14-01-2026)/StudentGradeChangeNotification/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace StudentGradeChangeNotificationApp
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Student student = new Student();
+      student.GradeChanged += (newGrade) =>
+      {
+        System.Console.WriteLine($"Grade Changed to {newGrade}");
+      };
+      if (int.TryParse(Console.ReadLine(), out int grade))
+      {
+        student.UpdateGrade(grade);
+      }
+      else
+      {
+        System.Console.WriteLine("Invalid grade");
+      }
+    }
+  }
+  public class Student
+  {
+    public event Action<int> GradeChanged;
+    public void UpdateGrade(int grade)
+    {
+      GradeChanged?.Invoke(grade);
+    }
+  }
+}
